d0558ec8dde859a5d20822bacee9c7c8b970bce3
Oskar Špaček <xspacek3@fi.muni.cz>
2021-03-16 09:06:06 +0100

    add Options for triggering Fizz and Buzz

--- FizzBuzz/fizzBuzz.py
+++ FizzBuzz/fizzBuzz.py
@@ -1,16 +1,16 @@
 def divisible(num, denominator):
 	return ((num % denominator) == 0)
 
-
-for i in range(1,20):
-    fizz = divisible(i, 3)
-    buzz = divisible(i, 5)
-    if (fizz == False and buzz == False):
-        print(i)
-    elif (fizz == True and buzz == False):
-        print("Fizz")
-    elif (fizz == False and buzz == True):
-        print("Buzz")
-    else: print("FizzBuzz")
+def fizzBuzz(top, trigger1, trigger2):        
+    for i in range(1,(top+1)):
+        fizz = divisible(i, trigger1)
+        buzz = divisible(i, trigger2)
+        if (fizz == False and buzz == False):
+            print(i)
+        elif (fizz == True and buzz == False):
+            print("Fizz")
+        elif (fizz == False and buzz == True):
+            print("Buzz")
+        else: print("FizzBuzz")
 
     
